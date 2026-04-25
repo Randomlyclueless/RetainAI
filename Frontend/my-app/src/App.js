358b09748c5b2d2a78e201390a783705a1125263
Randomlyclueless <144950366+Randomlyclueless@users.noreply.github.com>
2026-04-25 23:42:04 +0530

    landing Page improved

--- Frontend/my-app/src/App.js
+++ Frontend/my-app/src/App.js
@@ -1,20 +1,9 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Landing from "./pages/Landing";
-import Predict from "./pages/Predict";
-import Dashboard from "./pages/Dashboard"; // 👈 ADD THIS
+
+import RetainAI from "./pages/LandingPage";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/predictions" element={<Predict />} />
-        <Route path="/dashboard" element={<Dashboard />} /> {/* 👈 ADD THIS */}
-      </Routes>
-    </BrowserRouter>
-  );
+  return <RetainAI />;
 }
 
 export default App;
